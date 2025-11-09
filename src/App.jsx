eb48f3ab3f27d5be685c80919b61f3cad4f7eac9
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-11-09 17:27:18 +0530

    completed page - 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./components/Home.jsx";
 import Nav from "./components/Nav.jsx";
-import { Vid } from "./components/Vid.jsx";
+// import Vid  from "./components/Vid.jsx";
+import Vid from "./components/Vid.jsx";
 
 function App() {
   return (
     // <div className="h-screen w-full text-white bg-neutral-800">
-    <div className="h-screen w-full  bg-neutral-100 ">
+    <div className="h-screen w-full  bg-neutral-200 ">
       <Nav />
       <Home />
       <Vid />
--- src/components/Vid.jsx
+++ src/components/Vid.jsx
@@ -1,14 +1,24 @@
-export const Vid = () => {
+import { AiOutlinePlayCircle } from "react-icons/ai";
+const Vid = () => {
   return (
     <>
-      <div className="page2 w-full h-screen flex items-center justify-center bg-amber-100 ">
-        <div className="box  flex items-center justify-center ">
-          <div className="img w-full h-full bg-[url(public/solar.png)]">
-           
-            <button>Watch Video</button>
+      <div className="page2 bg-neutral-200 w-full h-screen flex items-center justify-center ">
+        <div className="box   flex items-center justify-center ">
+          <div className="img w-[65vw] relative ">
+            <img
+              className="w-full h-full object-center object-cover"
+              src="public/solar.png"
+              alt=""
+            />
+            <button className="absolute flex gap-2 items-center justify-center shadow-2xl top-[70%] rounded-full left-1/2 -translate-z-1/2 -translate-x-1/2 cursor-pointer transition-all duration-500 hover:scale-[1.03] px-4 py-3 bg-white ">
+              <AiOutlinePlayCircle className="size-5  font-light " />
+              <p className=" text-sm capitalize">Watch film</p>
+            </button>
           </div>
         </div>
       </div>
     </>
   );
 };
+
+export default Vid;
